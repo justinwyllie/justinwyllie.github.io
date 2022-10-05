013c5533cc05f5828cbe9414d3a24221d7424cb5
Justin Wyllie <justinwyllie@hotmail.co.uk>
2022-10-05 21:11:20 +0100

    Fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App.jsx";
+import './custom.scss';
+import "@babel/polyfill"; //for stripe
+import 'core-js/actual/structured-clone';
 
 
 
