076dfb6045b8e7414ce9b62cb99528812662684e
Justin Wyllie <justinwyllie@hotmail.co.uk>
2023-08-12 14:52:25 +0300

    trying to MAKE github update

--- src/Constants.js
+++ src/Constants.js
@@ -29,7 +29,7 @@ const LABELS = {
             keeptrying: {infinitive: "keep trying"},
             youscored: {infinitive: "you scored"},
             percentsign: {nominative: "%"},
-            answers_sent: {nominative: "Thank you. Your answers have been sent."}
+            answers_sent: {nominative: "Thank you. Your answers have been sent."},
 
             
         },
